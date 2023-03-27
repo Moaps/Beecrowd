fe2938060ba6da94161453bd843b752a383e0d5b
Clariana Nogueira <82335096+Moaps@users.noreply.github.com>
2023-03-27 12:31:21 -0300

    Update and rename 1387.c to BEE 1387.c

--- BEE 1387.c
+++ BEE 1387.c
@@ -18,8 +18,8 @@ int main(){
         maoEsquerda = 0;
         maoDireita = 0;
         scanf("%hu %hu", &maoEsquerda, &maoDireita);
-        if (maoEsquerda == 0 && maoDireita == 0) break;
-        if (maoEsquerda > 5 || maoDireita > 5) continue;
+        if (maoEsquerda == 0 && maoDireita == 0) break; // If the user inputs a 0 in both left and right hand, the code terminates
+        if (maoEsquerda > 5 || maoDireita > 5) continue; // If the user inputs a value greater than 6, the loop jumps to the next iteration, forcing a new input
 
         printf("%hu\n", maoEsquerda+maoDireita);
         
