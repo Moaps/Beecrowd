009131b969e3928563421d76475c52eaf8f11f20
Clariana Nogueira <82335096+Moaps@users.noreply.github.com>
2023-03-28 13:09:26 -0300

    Update BEE 1329.c

--- BEE 1329.c
+++ BEE 1329.c
@@ -22,7 +22,7 @@ int main(){
       
         if (qtdJogos == 0)
             break;            // If the inputed games amount is 0, the code is terminated
-        else if (qtdJogos > 1000 || qtdJogos < 1)
+        else if (qtdJogos > 10000 || qtdJogos < 1)
             break;           // If the inputed games amount is greater than 1000 or lesser than 1, the code jumps to the next iteration, forcing the user to input a value between those ranges
         
         do
